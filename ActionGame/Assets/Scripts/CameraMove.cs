e85965f528f34c7b8152ab6fbac59b8a44e63af4
MMR <mamoru0523@gmail.com>
2020-03-11 16:21:57 +0900

    Second

---
+++ ActionGame/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+	public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float posx = player.transform.position.x; //x of player
+        float posy = player.transform.position.y; //y of player
+        float posz = player.transform.position.z; //z of player
+
+        transform.position = new Vector3(posx, posy + 10, posz - 10);
+    }
+}
---
+++ ActionGame/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMove : MonoBehaviour
+{
+	public Rigidbody rb;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	if(Input.GetKey(KeyCode.UpArrow))
+    	{
+	        rb.AddForce(0 ,0, 5);
+    	}
+    	if(Input.GetKey(KeyCode.DownArrow))
+    	{
+	        rb.AddForce(0 ,0, -5);
+    	}
+    	if(Input.GetKey(KeyCode.RightArrow))
+    	{
+	        rb.AddForce(5 ,0, 0);
+    	}
+    	if(Input.GetKey(KeyCode.LeftArrow))
+    	{
+	        rb.AddForce(-5 ,0, 0);
+    	}
+        if(transform.position.y < -10)
+        {
+            transform.position = new Vector3(0,4,0);
+        }
+    }
+
+    void OnCollisionStay(Collision hit)
+    {
+        if(hit.gameObject.tag == "Enemy")
+        {
+            transform.position = new Vector3(0,4,0);
+        }
+        if(hit.gameObject.tag == "Goal")
+        {
+            SceneManager.LoadScene("GameClear");
+        }
+    }
+}
